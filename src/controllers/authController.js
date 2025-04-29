14d29caca2bd5a63b0881af7b800804302470b9f
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2025-04-29 09:34:12 -0300

    feat: create user routes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,9 @@
-import UserModel from '../models/userModel.js';
-import bcrypt from 'bcrypt';
+import UserModel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+
 
 class AuthController {
- // Listar todos os usuários
+// Listar todos os usuários
 async getAllUsers(req, res) {
     try {
       const users = await UserModel.findAll();
@@ -12,42 +13,41 @@ async getAllUsers(req, res) {
       res.status(500).json({ error: "Erro ao listar usuários" });
     }
   }
-
-  //Registar novo usuário
-  async register(req, res) {
+  //Registrar novo usuário
+  async register(req,res) {
     try {
-        const { name, email, password } = req.body;
+        const {name, email, password} = req.body;
 
-        //Validação básica 
+        //Validação básica
         if (!name || !email || !password) {
             return res.status(400).json({ error: "Os campos nome, email e senha são obrigatórios" });
-        }
-
-        //Verifica se o usuário já existe
+    } 
+    // Verificar se o usuario já existe
         const userExists = await UserModel.findByEmail(email);
         if (userExists) {
-            return res.status(400).json({ error: "Este email já está em uso!" });
+            return res.status(400).json({ error: "Este email já esta em uso!" });
         }
 
-        //Hash da senha
+        // Hashar da senha
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        //Criar objeto do usuário 
+        // Criar objeto do usuário
         const data = {
             name,
             email,
-            password: hashedPassword,
+            password: hashedPassword
         };
 
-        //Criar usuário
-        const users = await UserModel.create(data);
+        // Criar o usuário
+        const user = await UserModel.create(data);
 
-        return res.status(201).json({ message: "Usuário criado com sucesso!", 
-            users, });
-    
-    } catch (error) {
-        console.error("Erro ao criar um novo usuário:", error);
-        res.status(500).json({ error: "Erro ao criar um novo usuário" });
+        return res.status(201).json({
+            message: "Usuário criado com sucesso",
+            user
+            });
+        }catch (error) {
+            console.error("Erro ao criar um novo usuário:", error);
+            res.status(500).json({ error: "Erro ao criar um novo usuário" });
     }
   }
 }
